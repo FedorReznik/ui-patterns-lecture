d05eb9fe91067dc3aac3f61cd4ddd0d4383477ed
Fedor Reznik <fedor.reznik@gmail.com>
2025-06-05 19:49:17 +0200

    Add UC1 WinForms solution

---
+++ Code-behind/UserStory1/Main.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace UserStory1
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
