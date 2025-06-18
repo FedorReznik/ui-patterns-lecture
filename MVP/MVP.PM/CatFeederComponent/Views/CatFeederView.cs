4405f196afaef326bb4c99b56d5f3164fad4980e
Fedor Reznik <fedor.reznik@gmail.com>
2025-06-18 20:43:35 +0200

    router impl

--- MVP/MVP.PM/CatFeederComponent/Views/CatFeederView.cs
+++ MVP/MVP.PM/CatFeederComponent/Views/CatFeederView.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Windows.Forms;
 using MVP.Engine;
 
 namespace MVP.CatFeederComponent.Views
--- MVP/MVP.PM/Engine/IRouter.cs
+++ MVP/MVP.PM/Engine/IRouter.cs
@@ -1,17 +1,43 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Autofac.Features.Indexed;
+using JetBrains.Annotations;
 
 namespace MVP.Engine
 {
     public interface IRouter
     {
-        Task NavigateTo<T>(T presenter) where T : IPresenter;
+        Task NavigateTo<T>([NotNull] T presenter) where T : IPresenter;
     }
 
     public sealed class Router : IRouter
     {
-        public Task NavigateTo<T>(T presenter) where T : IPresenter
+        private readonly IIndex<Type, Func<IView>> _viewFactoriesMap;
+        private readonly IUIExecutor _uiExecutor;
+        private readonly INavigationHost _navigationHost;
+
+        public Router(
+            [NotNull] IIndex<Type, Func<IView>> viewFactoriesMap,
+            [NotNull] IUIExecutor uiExecutor,
+            [NotNull] INavigationHost navigationHost)
         {
-            throw new System.NotImplementedException();
+            _viewFactoriesMap = viewFactoriesMap ?? throw new ArgumentNullException(nameof(viewFactoriesMap));
+            _uiExecutor = uiExecutor ?? throw new ArgumentNullException(nameof(uiExecutor));
+            _navigationHost = navigationHost ?? throw new ArgumentNullException(nameof(navigationHost));
+        }
+
+        public async Task NavigateTo<T>(T presenter) where T : IPresenter
+        {
+            if (presenter == null) throw new ArgumentNullException(nameof(presenter));
+            
+            if(!_viewFactoriesMap.TryGetValue(typeof(T), out var viewFactory))
+                throw new InvalidOperationException($"Presenter '{typeof(T)}' does not mapped to any view");
+            
+            var view = await _uiExecutor.Execute(() => viewFactory());
+            
+            view.AttachPresenter(presenter);
+            
+            _navigationHost.ShowView(view.Render());
         }
     }
 }
